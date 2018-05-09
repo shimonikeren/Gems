7405218b349113cabdabc140b6936d8bfdd947c7
yelena guliyeva <gulbadam@gulbadam.com>
2018-05-09 11:09:26 -0700

    sign up CSS redirect after signUP

--- controllers/signup.js
+++ controllers/signup.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
                 registeredUser.lastname = req.body['lastname'] || null;
 
                 registeredUser.save();
-                passport.authenticate("local")(req, res, () => res.render("gems"))
+                passport.authenticate("local")(req, res, () => { res.redirect("/") })
             }
         )
     });
