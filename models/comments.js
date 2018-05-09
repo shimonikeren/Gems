8b8d120f1a83b2c29b18b539011da70e5b19cde0
Keren Shimoni <kereshimoni6@gmail.com>
2018-05-08 17:55:07 -0700

    comments, post has many comments

---
+++ models/comments.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+    var Comments = sequelize.define("Comments", {
+        Comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2, 1000]
+            }
+        }
+    });
+
+    Comments.associate = function(models) {
+        // We're saying that a Post should belong to a User
+        // A Post can't be created without an User due to the foreign key constraint
+        Comments.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+        // We're saying that a Post should belong to a Category
+        // A Post can't be created without an Category due to the foreign key constraint
+        Comments.belongsTo(models.UserPosts, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Comments;
+};
