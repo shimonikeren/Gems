0be0f5304ef26c79b0e506c01284224469c64fdf
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-10 16:21:13 -0700

    delete btn ajax working

---
+++ public/assets/js/app.js
@@ -0,0 +1,15 @@
+$(".deleteButton").on("click", function(event) {
+    event.preventDefault();
+    var itemId = $(this).data("id");
+    var query = "/gems/"+ itemId + "/delete";
+    console.log("ITEMID="+ itemId);
+    console.log("query="+ query);
+    $.ajax(query, {
+      type: "DELETE"
+    }).then(
+      function(something) {
+        console.log("worked");
+        location.reload();
+      }
+    );
+  });
