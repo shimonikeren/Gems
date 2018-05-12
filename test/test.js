c355a78ba1cd62fac89ca97249426204ded0685b
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-12 10:52:54 -0700

    fix div

---
+++ test/test.js
@@ -0,0 +1,19 @@
+// var Nightmare = require("nightmare");
+
+// //=================== test login process =================
+// new Nightmare({ show: true })
+//   .goto("http://localhost:8080/gems/all")
+//   .type("#login-name", "kjh")
+//   .type("#login-pw", "kjh")
+//   .screenshot("log-in.png")
+//   .click("#login-btn")
+//   .screenshot("logged-in.png")
+//   .end()
+//   .then(function() {
+//     console.log("Done!");
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
+
