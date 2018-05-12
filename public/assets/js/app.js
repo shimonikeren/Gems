d5c4c683033f7153431e3aa92be48023d523f653
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-12 12:38:47 -0700

    dislike btn works

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -30,3 +30,18 @@ $(".like-btn").on("click", function(event) {
     );
 });
 
+$(".dislike-btn").on("click", function(event) {
+    event.preventDefault();
+    var itemId = $(this).data("id");
+    var query = "/gems/vote/dislike/" + itemId;
+    console.log("ITEMID=" + itemId);
+    console.log("query=" + query);
+    $.ajax(query, {
+        type: "PUT"
+    }).then(
+        function(something) {
+            console.log("worked");
+            location.reload();
+        }
+    );
+});
